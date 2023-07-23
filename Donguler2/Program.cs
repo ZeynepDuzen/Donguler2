afc5cf16aea6ed23b456eae6bfe539ea401678a0
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-23 10:23:12 +0300

    Proje dosyası ekle.

---
+++ Donguler2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //mod kullanimi
+            //int sayi = 24 % 9;
+            //Console.WriteLine(sayi);
+
+            //mod kullanimi ornek;
+            //int sayi;
+            //Console.WriteLine("Lütfen bir sayi girin : ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+
+            //if (sayi % 2 == 0)
+            //{
+            //    Console.WriteLine("Cift sayi girdiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tek sayi girdiniz");
+            //}
+
+
+            //Dongulerin icerisinde karar yapilari kullanma;
+            //1-20 arası 3'e tam bolunen sayilarin listesini veren kod blogu;
+
+            int i;
+
+            for (i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Donguler2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Donguler2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Donguler2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("1946e891-002b-4864-9f50-5b1fcf771ab5")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
